8871a9f1e03904fb0dada297ef2f3109db1c7549
xmly <xmly@xmlydeMacBook-Pro.local>
2021-02-08 17:37:48 +0800

    style: 用 style 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,14 +1,53 @@
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      {/* <Nav>Nav</Nav> */}
+      <Main>
+        <ProjectListScreen />
+      </Main>
+      {/* <Aside>Aside</Aside> */}
+      {/* <Footer>Footer</Footer> */}
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas: "header header header" "main main main";
+  height: 100vh;
+  grid-gap: 5rem;
+`;
+
+const Header = styled(Row)`
+  background-color: lightblue;
+  grid-area: header;
+`;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  background-color: lightblue;
+  grid-area: main;
+`;
 
 export default AuthenticatedApp;
