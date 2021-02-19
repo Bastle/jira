44e02e44ccbd048383d1d1d1f1bb6d09978ade59
xmly <xmly@xmlydeMacBook-Pro.local>
2021-02-19 16:24:39 +0800

    style: 优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -48,10 +48,11 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List list={list} users={users} />
-    </div>
+    </Container>
   );
 };
 
